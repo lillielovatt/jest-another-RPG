1e41336d985005735126c7da269ddcd82b017de2
lillielove <lillielove@gmail.com>
2022-07-05 17:16:53 -0700

    add prototypes to Player constructor, to confirm inventory and get player stats. DO NOT use arrow functions because it prevents the use of the 'this' functionality.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,6 @@
 const Player = require("../lib/Player.js");
 const Potion = require("../lib/Potion");
 jest.mock("../lib/Potion");
-console.log(new Potion());
 
 //want a player to have a name,and three numerical values - health, strength, agility
 
@@ -17,3 +16,20 @@ test("creates a player object", () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player=new Player("Dave");
+
+    expect(player.getStats()).toHaveProperty("potions");
+    expect(player.getStats()).toHaveProperty("strength");
+    expect(player.getStats()).toHaveProperty("agility");
+    expect(player.getStats()).toHaveProperty("health");
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player("Dave");
+    console.log(player.inventory);
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory=[];
+    expect(player.getInventory()).toEqual(false);
+})
---
+++ notes.js
@@ -0,0 +1,16 @@
+// this.getStats=function(){
+//     return{
+//         potions: this.inventory.length,
+//         health: this.health,
+//         strength: this.strength,
+//         agility: this.agility
+//     };
+// };
+
+// this.getInventory=function(){
+//     if(this.inventory.length) return this.inventory
+//     else return false
+    
+//     // this.inventory.length ? this.inventory : false;
+//     // why didn't this work?
+// }
