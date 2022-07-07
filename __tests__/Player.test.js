c0dd3ec1073c48742bb8e6414e3539ba11b27bab
lillielove <lillielove@gmail.com>
2022-07-06 17:40:40 -0700

    nearly done with Player constructor, more tests written for methods added through prototypes. Next, tackle the Enemy constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,4 +32,26 @@ test("gets inventory from player or returns false", () => {
     expect(player.getInventory()).toEqual(expect.any(Array));
     player.inventory=[];
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets player's health value", () => {
+    const player = new Player ("Dave");
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString())) //why not toEqual? or number? because we might need flexibility to change how the player's health will be displayed
+});
+
+test("checks if player is alive or not", () => {
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health=0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("checks if correct amount of health is subtracted", () => {
+    const player=new Player("Dave");
+    const oldHealth = player.health;
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth-5);
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+});
